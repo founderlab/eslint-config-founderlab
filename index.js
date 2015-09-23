ebe6cc2909f08e8a623ee55e63b77c74b1e75bc7
gwilym <gwilym@gwilymh.com>
2015-09-23 10:38:01 +1000

    rules

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+// Use .json styling for this file
+
 module.exports = {
-  extends: 'airbnb',
-  rules: {
-    semi: [2, 'never']
-  },
+  "extends": "airbnb",
+  "rules": {
+    'no-console': 0,                 // http://eslint.org/docs/rules/no-console
+    "semi": [2, "never"]              // http://eslint.org/docs/rules/semi
+  }
 }
