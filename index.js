42ae77ef888d0b17e264ef20d0340a62be6fb3f2
gwilym <gwilym@gwilymh.com>
2015-09-23 11:57:46 +1000

    switch indent

--- index.js
+++ index.js
@@ -3,11 +3,12 @@
 module.exports = {
   "extends": "airbnb",
   "rules": {
-    "camelcase": 0,
-    "padded-blocks": 0,
-    "no-shadow": 0,
-    "space-infix-ops": 0,
-    'no-console': 0,                 // http://eslint.org/docs/rules/no-console
-    "semi": [2, "never"]              // http://eslint.org/docs/rules/semi
+    "camelcase": 0,                       // http://eslint.org/docs/rules/camelcase
+    "padded-blocks": 0,                   // http://eslint.org/docs/rules/padded-blocks
+    "no-shadow": 0,                       // http://eslint.org/docs/rules/no-shadow
+    "space-infix-ops": 0,                 // http://eslint.org/docs/rules/space-infix-ops
+    'indent': [2, 2, {"SwitchCase": 1}],  // http://eslint.org/docs/rules/indent
+    'no-console': 0,                      // http://eslint.org/docs/rules/no-console
+    "semi": [2, "never"]                  // http://eslint.org/docs/rules/semi
   }
 }
