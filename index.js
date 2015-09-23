ef274fcffbf514d8fc2be0257be69cfacc8e1b9f
gwilym <gwilym@gwilymh.com>
2015-09-23 11:16:55 +1000

    disable space-infix-ops for function arg defaults

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 module.exports = {
   "extends": "airbnb",
   "rules": {
+    "space-infix-ops": 0,
     'no-console': 0,                 // http://eslint.org/docs/rules/no-console
     "semi": [2, "never"]              // http://eslint.org/docs/rules/semi
   }
