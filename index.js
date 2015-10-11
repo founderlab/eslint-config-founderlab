b3adedb6398073ddacb9634d925b33e1e6825870
gwilym <gwilym@gwilymh.com>
2015-10-12 10:27:58 +1100

    work with npm@2.x

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-// Use .json styling for this file
+var merge = require('lodash/object/merge')
+var airbnb = require('eslint-config-airbnb')
 
-module.exports = {
-  "extends": "airbnb",
+module.exports = merge(airbnb, {
+  // "extends": "airbnb",
   "rules": {
     "spaced-comment": 0,                  // http://eslint.org/docs/rules/spaced-comment
     "camelcase": 0,                       // http://eslint.org/docs/rules/camelcase
     "padded-blocks": 0,                   // http://eslint.org/docs/rules/padded-blocks
     "no-shadow": 0,                       // http://eslint.org/docs/rules/no-shadow
     "space-infix-ops": 0,                 // http://eslint.org/docs/rules/space-infix-ops
-    'indent': [2, 2, {"SwitchCase": 1}],  // http://eslint.org/docs/rules/indent
-    'no-console': 0,                      // http://eslint.org/docs/rules/no-console
+    "indent": [2, 2, {"SwitchCase": 1}],  // http://eslint.org/docs/rules/indent
+    "no-console": 0,                      // http://eslint.org/docs/rules/no-console
     "semi": [2, "never"]                  // http://eslint.org/docs/rules/semi
   }
-}
+})
