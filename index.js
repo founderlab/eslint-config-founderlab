08a6796f5e039f6d347cecbe1799c69ff8639b1a
gwilym <gwilym@gwilymh.com>
2017-10-09 13:53:37 +1100

    Dependencies and rules for eslint@4 eslint-config-airbnb@16

--- index.js
+++ index.js
@@ -19,5 +19,31 @@ module.exports = _.merge(airbnb, {
     "no-console": 0,                      // http://eslint.org/docs/rules/no-console
     "semi": [2, "never"],                 // http://eslint.org/docs/rules/semi
     "no-unexpected-multiline": 2,         // http://eslint.org/docs/rules/no-unexpected-multiline
+
+    // Added with eslint-config-airbnb@16
+    "arrow-parens": [2, "as-needed"],
+    "consistent-return": 0,
+    "prefer-destructuring": 0,
+    "newline-per-chained-call": 0,
+    "no-param-reassign": 0,
+    "no-mixed-operators": 0,
+    "eqeqeq": 0,
+    "function-paren-newline": 0,
+    "global-require": 0,
+    "max-len": 0,
+    "no-case-declarations": 0,
+    "no-return-assign": 0,
+    "no-unneeded-ternary": 0,
+    "object-curly-newline": 0,
+    "object-curly-spacing": [2, "never"],
+    "prefer-template": 0,
+
+    "import/no-named-as-default": 0,
+    "import/no-named-as-default-member": 0,
+    "import/no-extraneous-dependencies": 0,
+
+    "react/jsx-filename-extension": 0,
+    "react/prefer-stateless-function": 0,
+    "react/forbid-prop-types": 0,
   }
 })
